6f45a8bf8728904e53f708826afe346d107b1b1e
Dai ZeHua <62000568+daizehua1@users.noreply.github.com>
2024-02-27 17:30:32 +0800

    Add files via upload

---
+++ vllm.py
@@ -0,0 +1,76 @@
+# 导入所需的库
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
+import torch
+import streamlit as st
+from modelscope import AutoTokenizer, snapshot_download
+from auto_gptq import AutoGPTQForCausalLM
+
+# 在侧边栏中创建一个标题和一个链接
+with st.sidebar:
+    st.markdown("## DeepSeek LLM")
+    "[开源大模型食用指南 self-llm](https://github.com/datawhalechina/self-llm.git)"
+    # 创建一个滑块，用于选择最大长度，范围在0到1024之间，默认值为512
+    max_length = st.slider("max_length", 0, 1024, 512, step=1)
+
+# 创建一个标题和一个副标题
+st.title("💬 DeepSeek Chatbot")
+st.caption("🚀 A streamlit chatbot powered by Self-LLM")
+
+# 定义模型路径
+mode_name_or_path = '/root/autodl-tmp/deepseek-ai/deepseek-llm-7b-chat'
+
+
+# 定义一个函数，用于获取模型和tokenizer
+@st.cache_resource
+def load_model_tokenizer(model_path):
+    """
+    Load model and tokenizer based on the given model name or local path of downloaded model.
+    """
+    tokenizer = AutoTokenizer.from_pretrained(model_path,
+                                              trust_remote_code=True,
+                                              use_fast=False,
+                                              lagecy=False)
+    tokenizer.padding_side = "left"
+    tokenizer.pad_token_id = tokenizer.convert_tokens_to_ids("<｜end▁of▁sentence｜>")
+    tokenizer.eos_token_id = tokenizer.convert_tokens_to_ids("<｜end▁of▁sentence｜>")
+
+    model = AutoGPTQForCausalLM.from_quantized(model_path,
+                                               inject_fused_attention=False,
+                                               inject_fused_mlp=False,
+                                               use_safetensors=True,
+                                               use_cuda_fp16=True,
+                                               disable_exllama=False,
+                                               device_map='auto'  # Support multi-gpus
+                                               )
+    return model, tokenizer
+
+
+# 加载Chatglm3的model和tokenizer
+tokenizer, model = load_model_tokenizer()
+
+# 如果session_state中没有"messages"，则创建一个包含默认消息的列表
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "有什么可以帮您的？"}]
+
+# 遍历session_state中的所有消息，并显示在聊天界面上
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+# 如果用户在聊天输入框中输入了内容，则执行以下操作
+if prompt := st.chat_input():
+    # 将用户的输入添加到session_state中的messages列表中
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # 在聊天界面上显示用户的输入
+    st.chat_message("user").write(prompt)
+
+    # 构建输入
+    input_tensor = tokenizer.apply_chat_template(st.session_state.messages, add_generation_prompt=True,
+                                                 return_tensors="pt")
+    # 通过模型获得输出
+    outputs = model.generate(input_tensor.to(model.device), max_new_tokens=max_length)
+    # 解码模型的输出，并去除特殊标记
+    response = tokenizer.decode(outputs[0][input_tensor.shape[1]:], skip_special_tokens=True)
+    # 将模型的输出添加到session_state中的messages列表中
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    # 在聊天界面上显示模型的输出
+    st.chat_message("assistant").write(response)
